260cd8e6667699b49de0f67f445194c5fa9ee896
YossiMoalem <moalem.yossi@gmail.com>
2014-04-19 04:34:10 -0400

    Update analize script

--- analyzer/analyzer.pl
+++ analyzer/analyzer.pl
@@ -1,94 +1,119 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use constant AddressDelemeter => ":";
+use constant StackHitDelemeter => "=";
 
 my %gStackCounter = ();
 my %gAddress2Name = ();
 my %gName2Addresss = ();
 
-my $cDelemeter = ":";
-
-my $gLogFilename = shift;
-my $gAppName = shift;
-#TODO: check values!
-
+my $gAppName;
 my $gLogFileHnd;
 
-open $gLogFileHnd, "<", $gLogFilename if defined  $gLogFilename;
-die "Cannot open input file $gLogFilename. Exiting" unless defined $gLogFilename;
-#
-#open $gLogFileHnd, ">>", $logFilePath if defined $logFilePath ; 
-#print "Cannot open log file. writing to STDOUT \n " unless defined $gLogFileHnd;
-#select ($gLogFileHnd) if defined $gLogFileHnd;
+init (@ARGV);
+readFile ();
+resolveFuncNames ();
+printOutput ();
 
-while (my $curLine = <$gLogFileHnd>)
+sub readFile 
 {
-   chomp $curLine;
-#TODO: verify input
-   $curLine =~ /^(.+)=(\d+)\s*$/ ;
-   my $stack = $1;
-   my $hits = $2;
-   #remove (null), (nil) ...
-   $stack =~ s/\([niul]+\)/0/g;
-
-   #count hits:
-   if (!defined $gStackCounter{$stack} )
+   while (my $curLine = <$gLogFileHnd>)
    {
-      $gStackCounter{$stack} = 0;
-   }
-   $gStackCounter{$stack} = $gStackCounter{$stack} + $hits;
+      chomp $curLine;
+      #remove (null), (nil) ...
+      $curLine=~ s/\([niul]+\)/0/g;
+#                     --Zero Addr : --          | Real Addr    :                 --          =                  hits   
+      $curLine =~ /^((0[${\AddressDelemeter()}]?|0x[0-9a-f]+[${\AddressDelemeter()}]?)*)${\StackHitDelemeter()}(\d+)\s*$/ or die "Error: line $curLine does not seems to be a valid profiler log entry";
+      my $stack = $1;
+      my $hits = $3;
 
-   #keep all addresses:
-   my @frames = split ($cDelemeter, $stack);
-   foreach my $frame (@frames)
-   {
-      $gAddress2Name{$frame} = "" unless $frame eq "0";
-   }
+      #count hits:
+      if (!defined $gStackCounter{$stack} )
+      {
+         $gStackCounter{$stack} = 0;
+      }
+      $gStackCounter{$stack} = $gStackCounter{$stack} + $hits;
 
+      #keep all addresses:
+      my @frames = split (AddressDelemeter, $stack);
+      foreach my $frame (@frames)
+      {
+         $gAddress2Name{$frame} = "" unless $frame eq "0";
+      }
+   }
 }
 
-#resolve the addresses to function names:
-while (my ($address, $_ ) = each %gAddress2Name)
+#Resolve the addresses to function names
+#if we encounter two addresses to the same function
+#replace one with the other, so each function will have
+#one, distinct address.
+sub resolveFuncNames
 {
-   my @resolved = `addr2line -f -C -e $gAppName  $address`;
-   chomp ($resolved[0]);
-
-   if (!defined $gName2Addresss{$resolved[0]})
+   while (my ($address, $_ ) = each %gAddress2Name)
    {
-      # If this is the first time we see this function, add it to the dictionary
-      $gAddress2Name{$address} = $resolved[0];
-      $gName2Addresss{$resolved[0]} = $address;
-   } else {
-      # We already got this function. Replace the new address, with the old one
-      #print "replacing $address with $gName2Addresss{$resolved[0]} \n";
-      foreach my $oldStack (keys  %gStackCounter)
+      my @resolved = `addr2line -f -C -e $gAppName  $address`;
+      chomp ($resolved[0]);
+
+      if (!defined $gName2Addresss{$resolved[0]})
       {
-         my $changed = (my $newStack = $oldStack) =~  s/$address/$gName2Addresss{$resolved[0]}/;
-         if ( $changed )
+# If this is the first time we see this function, add it to the dictionary
+         $gAddress2Name{$address} = $resolved[0];
+         $gName2Addresss{$resolved[0]} = $address;
+      } else {
+# We already got this function. Replace the new address, with the old one
+#print "DEBUG: replacing $address with $gName2Addresss{$resolved[0]} \n";
+         foreach my $oldStack (keys  %gStackCounter)
          {
-            if (!defined $gStackCounter{$newStack} ) 
+            my $changed = (my $newStack = $oldStack) =~  s/$address/$gName2Addresss{$resolved[0]}/;
+            if ( $changed )
             {
-               $gStackCounter{$newStack} = 0;
+               if (!defined $gStackCounter{$newStack} ) 
+               {
+                  $gStackCounter{$newStack} = 0;
+               }
+               $gStackCounter{$newStack} += delete $gStackCounter{$oldStack};
             }
-            $gStackCounter{$newStack} += delete $gStackCounter{$oldStack};
          }
+         my @newStack  = keys %gStackCounter;
+
+#Create the updated hash
+         @gStackCounter{@newStack} = delete @gStackCounter{keys %gStackCounter}; 
+
+#Remove the old address from the map
+         delete $gAddress2Name{$address};
       }
-      my @newStack  = keys %gStackCounter;
-
-      #Create the updated hash
-      @gStackCounter{@newStack} = delete @gStackCounter{keys %gStackCounter}; 
-
-      #Remove the old address from the map
-      delete $gAddress2Name{$address};
    }
 }
 
 #print the output.
-while (my ($addr, $name) = each %gAddress2Name)
+sub printOutput
 {
-   print "$addr=$name \n";
+   while (my ($addr, $name) = each %gAddress2Name)
+   {
+      print "$addr=$name \n";
+   }
+   while (my ($stack, $hits) = each %gStackCounter)
+   {
+      print "$stack=$hits \n";
+   }
 }
-while (my ($stack, $hits) = each %gStackCounter)
+
+sub init
 {
-   print "$stack=$hits \n";
+   my $logFilename      = shift;
+   $gAppName            = shift;
+   my $outputFileName   = shift;
+
+   if (! defined $gAppName)
+   {
+      print "Usage: $0 <Log file> <executable> [Output file] \n";
+      exit -1;
+   }
+   open $gLogFileHnd, "<", $logFilename if defined  $logFilename;
+   die "Cannot open input file $logFilename. Exiting" unless defined $logFilename;
+
+   open my $outputFileHnd, ">>", $outputFileName or die "Cannot open log file $outputFileName" if defined $outputFileName ;
+   select ($outputFileHnd) if defined $outputFileHnd;
+
 }
